0314f11428eedb80945717fb4ff78614aaa5def0
FruFru01 <martin.fruhman@gmail.com>
2015-05-15 15:55:47 +0200

    new initial commit

---
+++ android/src/at/fhj/itm13/projectwork/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package at.fhj.itm13.projectwork.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import at.fhj.itm13.projectwork.ShooterGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ShooterGame(), config);
+	}
+}
