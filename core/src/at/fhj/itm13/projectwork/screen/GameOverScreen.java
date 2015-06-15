ea371af60078916ac4041dc7058928be19a0e0c2
FruFru01 <martin.fruhman@gmail.com>
2015-06-15 10:53:06 +0200

    added button images

--- core/src/at/fhj/itm13/projectwork/screen/GameOverScreen.java
+++ core/src/at/fhj/itm13/projectwork/screen/GameOverScreen.java
@@ -11,29 +11,13 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
 public class GameOverScreen extends Screen{
 
-	private Stage stage;
-	private TextButton button;
-	private TextButtonStyle textButtonStyle;
-	private BitmapFont font;
+	private Stage stage;	
 	private Skin skin;
-	private TextureAtlas buttonAtlas;
-	
 	
 	@Override
 	public void create() {
-		//Button
 		stage = new Stage();
-        Gdx.input.setInputProcessor(stage);
-        font = new BitmapFont();
-        skin = new Skin();
-        //buttonAtlas = new TextureAtlas(Gdx.files.internal("buttons/buttons.pack"));
-        //skin.addRegions(buttonAtlas);
-        textButtonStyle = new TextButtonStyle();
-        textButtonStyle.font = font;/*
-        textButtonStyle.up = skin.getDrawable("up-button");
-        textButtonStyle.down = skin.getDrawable("down-button");
-        textButtonStyle.checked = skin.getDrawable("checked-button");*/
-        button = new TextButton("Button1", textButtonStyle);
+		skin = new Skin();
 	}
 
 	@Override
@@ -42,6 +26,8 @@ public class GameOverScreen extends Screen{
 
 	@Override
 	public void render(SpriteBatch sb) {
+		stage.act();
+		stage.draw();
 	}
 
 	@Override
@@ -51,7 +37,7 @@ public class GameOverScreen extends Screen{
 
 	@Override
 	public void dispose() {
-		
+		stage.dispose();
 	}
 
 	@Override
